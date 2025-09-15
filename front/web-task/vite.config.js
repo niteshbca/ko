e917ce60a9957ad52c6ea637d173de9d8ead97f3
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-09-15 23:44:49 +0530

    Update vite.config.js

--- front/web-task/vite.config.js
+++ front/web-task/vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 import { RiTailwindCssFill } from 'react-icons/ri'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-  // tailwindcss(),
-],
-// server:{
-//     port:5173,
-//   }
+    // tailwindcss(),
+  ],
+  server: {
+    host: true, // 0.0.0.0 par bind karega
+    allowedHosts: ['incentives.expert'], // <-- yahan apna domain add kiya
+    port: 5173
+  }
 })
